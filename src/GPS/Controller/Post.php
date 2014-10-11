b63737a98f18db70d46523b0ec263deb0edeb6b8
Gustavo Seganfredo <gustavosf@gmail.com>
2014-10-11 15:38:59 -0300

    Incluído bootstrap simples

---
+++ src/GPS/Controller/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GPS\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class Post
+ *
+ * @package GPS\Controller
+ */
+class Post
+{
+
+	/**
+	 * Splash Page
+	 *
+	 * @param Request	 $request
+	 * @param Application $app
+	 *
+	 * @return mixed
+	 */
+	public function indexAction(Request $request, Application $app)
+	{
+		return $app['twig']->render('index.html.twig', array());
+	}
+
+}
---
+++ src/Routes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Silex\Application\TwigTrait;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+
+/** @var Silex\Application $app * */
+$app->get('/', 'GPS\Controller\Post::indexAction');
+$app->get('/index.html', 'GPS\Controller\Post::indexAction');
+
+## Error Handlers ##############################################################
+#
+$app->error(function (\Exception $e, $code) use ($app) {
+	switch ($code) {
+		case 404:
+			$message = $app['twig']->render('error404.html.twig');
+			break;
+		default:
+			$message = 'We are sorry, but something went terribly wrong.';
+	}
+
+	if ($app['debug']) {
+		$message .= ' Error Message: ' . $e->getMessage();
+	}
+
+	return new Response($message, $code);
+});
+
+return $app;
